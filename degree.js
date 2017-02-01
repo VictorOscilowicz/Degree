ed7e69dd3e0ce0114ae7437248a21d7f7de8fed4
VictorOscilowicz <7voscilowicz@stpatrick.org>
2017-02-01 11:25:00 -0600

    Add files via upload

---
+++ degree.js
@@ -0,0 +1,28 @@
+const Matrix = require("matrix-js");
+
+let A = Matrix([
+    [1,1,0,1,0],
+    [1,0,1,0,0],
+    [0,1,0,0,0],
+    [1,0,0,0,1],
+    [0,0,0,1,0]
+]);
+//Code here
+function degree(mtrx,v){
+  let [sum]() = 0;
+  let [degreelist]([],0);
+
+  for(s=0;s){
+    return [sum]();
+  }
+}
+
+function degreecentrality(){
+  let
+}
+
+//Challenge
+
+//Run test code with ctrl+shift+p  then type in "script"
+console.log(degree(A,3));
+console.log(degreecentrality(A,2));
